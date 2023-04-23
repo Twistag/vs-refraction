7a5f0d4c2bfeb651180fff91d98267ba9acce016
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-23 18:02:44 -0400

    Revert "update version to 1.0.1"
    This reverts commit 6443d31852cc03cf23f141e0b0879dc081991daa.

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -11,7 +11,7 @@ namespace Refraction
         public const string Name = "Refraction";
         public const string Description = "Learn, improve and generate code with AI.";
         public const string Language = "en-US";
-        public const string Version = "1.0.1";
+        public const string Version = "1.0.0";
         public const string Author = "Hayden Bleasel";
         public const string Tags = "";
     }
